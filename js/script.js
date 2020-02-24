ac4f891c414f06a1963d1ff304f89f5bb3b6c52c
apip <apip@gitlab.lab>
2020-02-24 15:18:53 +0700

    add style

--- js/script.js
+++ js/script.js
@@ -4,18 +4,17 @@
 const tanah = document.querySelectorAll('.tanah');
 /* ambil element tikus */
 const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
+
 let tanahSebelumnya;
+let selesai;
+let skor;
 
-
-/* funct untuk menampilkan tikus */
-function tampilkanTikus(tanah) {
-    const t = randomTanah(tanah);
-    tRandom.classList.add('muncul');
-}
 
 /* funct untuk mengambil bilangan random tanah */
 function randomTanah(tanah) {
-    const t = Math.floor(Math.random() * tanah.length); /* bulatkan kebawah */
+    const t = Math.floor(Math.random() * tanah.length);
     const tRandom = tanah[t];
     if (tRandom == tanahSebelumnya) {
         randomTanah(tanah);
@@ -23,3 +22,41 @@ function randomTanah(tanah) {
     tanahSebelumnya = tRandom;
     return tRandom;
 }
+/* funct untuk mengambil bilangan random waktu */
+function randomWaktu(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+/* funct untuk menampilkan tikus */
+function munculkanTikus() {
+    const tRandom = randomTanah(tanah);
+    const wRandom = randomWaktu(300, 1000);
+    tRandom.classList.add('muncul');
+
+    setTimeout(() => {
+        tRandom.classList.remove('muncul');
+        if (!selesai) {
+            munculkanTikus();
+        }
+    }, wRandom);
+}
+/* funct untuk button */
+function mulai() {
+    selesai = false;
+    skor = 0;
+    papanSkor.textContent = 0;
+    munculkanTikus();
+    setTimeout(() => {
+        selesai = true;
+    }, 10000);
+}
+/* function untuk mukul tikus */
+function pukul() {
+    skor++;
+    this.parentNode.classList.remove('muncul'); /* hilangkan class muncul */
+    pop.play();
+    papanSkor.textContent = skor; /* ubah skor di papan skor */
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul);
+});
